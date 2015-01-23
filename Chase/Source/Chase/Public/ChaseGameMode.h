37339fa1ec606e124b14f4e35798c7f209e989ce
unknown <sjb4138@GDD-60.main.ad.rit.edu>
2015-01-23 17:02:07 -0500

    initial import of project

---
+++ Chase/Source/Chase/Public/ChaseGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameMode.h"
+#include "ChaseGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AChaseGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	AChaseGameMode(const FObjectInitializer& ObjectInitializer);
+};
+
+
+
